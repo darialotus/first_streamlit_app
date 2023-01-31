2d38b8fee0617b28cb26c1b9fcd91c08b4ed48c3
darialotus <124063598+darialotus@users.noreply.github.com>
2023-01-31 17:27:42 +0200

    Update streamlit_app.py
    Multi-select

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,12 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 #Let's start using another Python package library.
 import pandas
+
 #We want pandas to read our CSV file from that S3 bucket so we use a pandas function called read_csv  to pull the data into a dataframe we'll call my_fruit_list. 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-#we will ask the streamlit library to display it 
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
 streamlit.dataframe(my_fruit_list)
