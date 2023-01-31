afcc90bbe04ad4ef25d9ecd7fcaf7ace1d77e618
darialotus <124063598+darialotus@users.noreply.github.com>
2023-01-31 17:28:49 +0200

    Update streamlit_app.py
    Choose the Fruit Name Column as the Index

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,8 @@ import pandas
 #We want pandas to read our CSV file from that S3 bucket so we use a pandas function called read_csv  to pull the data into a dataframe we'll call my_fruit_list. 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
